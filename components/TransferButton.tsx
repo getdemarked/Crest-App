712789c5aa53825ab9717779f252bc2dda358730
Allan Brando Catayoc <31358319+itsnalla@users.noreply.github.com>
2023-12-06 19:40:44 +0800

    added fees every transaction

--- components/TransferButton.tsx
+++ components/TransferButton.tsx
@@ -1,5 +1,5 @@
-import { Web3Button, useContract } from "@thirdweb-dev/react";
-import { TRANSFER_CONTRACT_ADDRESS } from "../const/addresses";
+import { Web3Button, useContract,useAddress } from "@thirdweb-dev/react";
+import { TRANSFER_CONTRACT_ADDRESS, FEE_TOKEN_ADDRESS, FEE_AMOUNT, FEE_RECEIVER } from "../const/addresses";
 import { ethers } from "ethers";
 import { useToast } from "@chakra-ui/react";
 import styles from "../styles/CashInOutForm.module.css";
@@ -18,21 +18,38 @@ export default function TransferButton({
   message,
 }: Props) {
   const toast = useToast();
+  const address = useAddress();
 
   const { contract: tokenContract } = useContract(tokenAddress, "token");
-
   const { contract: transferContract } = useContract(
     TRANSFER_CONTRACT_ADDRESS
   );
 
   const handleTransfer = async () => {
     try {
+      // Check if the user has enough balance to cover the fee
+      const userBalance = await tokenContract?.call("balanceOf", [address as string]);
+
+      // Ensure that userBalance is defined before accessing its properties
+      if (userBalance && userBalance[0] && userBalance[0].lt(ethers.utils.parseEther(FEE_AMOUNT))) {
+        // Display a toast message if the user doesn't have enough balance
+        toast({
+          title: "Insufficient Balance",
+          description: "You don't have enough XPHP to cover the fees.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+        return;
+      }
+
       // Set the allowance and initiate the transfer
       await tokenContract?.setAllowance(
         TRANSFER_CONTRACT_ADDRESS,
         ethers.utils.parseEther(amount).toString()
       );
 
+      // Transfer the main amount
       await transferContract?.call("transfer", [
         tokenAddress,
         receiver,
@@ -40,12 +57,20 @@ export default function TransferButton({
         message,
       ]);
 
+      // Transfer the fee amount
+      await transferContract?.call("transfer", [
+        FEE_TOKEN_ADDRESS,
+        FEE_RECEIVER,
+        ethers.utils.parseEther(FEE_AMOUNT),
+        "Transaction Fee",
+      ]);
+
       // Display a success toast
       toast({
         title: "Transfer Successful",
-        description: "You have successfully transferred an asset",
+        description: "You have successfully transferred an asset.",
         status: "success",
-        duration: 5000000000000, // Adjust the duration as needed
+        duration: 5000,
         isClosable: true,
       });
     } catch (error) {
@@ -55,7 +80,7 @@ export default function TransferButton({
         title: "Transfer Error",
         description: "Something went wrong while transferring.",
         status: "error",
-        duration: 5000000000000, // Adjust the duration as needed
+        duration: 5000,
         isClosable: true,
       });
     }
--- const/addresses.ts
+++ const/addresses.ts
@@ -1,6 +1,10 @@
 export const TRANSFER_CONTRACT_ADDRESS = '0xF9DeC9D4dd19b654857D016E40B7b45DfBa8E337';
 export const CALIM_TOKEN_CONTRACT_ADDRESS = '0xf4ED0ba75DfA2D64828e9F6cd280fB660Bc09908';
 
+export const FEE_TOKEN_ADDRESS = '0xf4ED0ba75DfA2D64828e9F6cd280fB660Bc09908';
+export const FEE_AMOUNT = '10';
+export const FEE_RECEIVER = '0x723A159B280E23889E78ae3c397b52cca21eCd3b';
+
 export const HERO_IMAGE_URL = 'ipfs://QmUWVRuyFuVfo45estsJt2VaQUSEc4uwcPoeWFaeYxERua/seanwatase_isometric_crypto_transferring_from_one_wallet_to_ano_96084ff1-fe03-4dfd-8783-a3ecc85a24da.png';
 export const FEATURES_IMAGE_URL = 'ipfs://QmcTgfb2AXj1TcVj4i9jSgZc8A8LhxHYCXbu5Mngsx7iVT/Coins.png';
 export const CLAIM_TOKEN_IMAGE = 'ipfs://QmUYpRzQE6bebNuuVriyd2n163wAKu1pNRusqQraeRU5bX/seanwatase_isometric_black_crypto_coin_transparent_background_3dcbfe44-babc-45f5-9d43-881d85d2293d.png';
